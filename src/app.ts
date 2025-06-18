fdd2c940eae1ca84cf709b4b7bfd3134489cf3da
om rajpal <omrajpal.exe@gmail.com>
2025-06-18 20:29:30 +0530

    fixed page not found error

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(Logger);
 app.use("/api/v1", router);
-app.use('/api/v1/qrcode', express.static('qr'))
-app.use(NotFoundHandler);
-app.use(InternalErrorHandler);
+app.use("/api/v1/qrcode", express.static("qr"));
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
+
+app.use(NotFoundHandler);
+app.use(InternalErrorHandler);
 
 export default app;
